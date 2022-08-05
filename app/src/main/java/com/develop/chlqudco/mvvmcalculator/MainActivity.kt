ee1144866120b1042bd2553c2a7bb0a755d6c2d8
choebyeongchae <chlqudco2@naver.com>
2022-08-06 00:58:32 +0900

    test2

--- app/src/main/java/com/develop/chlqudco/mvvmcalculator/MainActivity.kt
+++ app/src/main/java/com/develop/chlqudco/mvvmcalculator/MainActivity.kt
@@ -2,10 +2,13 @@ package com.develop.chlqudco.mvvmcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this,"안녕하세요",Toast.LENGTH_SHORT).show()
     }
 }
